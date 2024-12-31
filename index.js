9ae5d7f79ed6d9636c28ef37f6d05af48d94a5d2
Ridoy5315 <ridoy.st99@gmail.com>
2024-12-31 16:03:06 +0800

    successfully created the jeson web token in server side

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 require('dotenv').config()
 const port = process.env.PORT || 3000;
 
-app.use(cors());
-app.use(express.json());
+const corsOptions = {
+  origin: ['http://localhost:5173'],
+  credentials: true,
+  optionalSuccessStatus: 200,
+}
+
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(cookieParser())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oggyj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -20,16 +29,77 @@ const client = new MongoClient(uri, {
   }
 });
 
+// verifyToken
+const verifyToken = (req, res, next) => {
+  const token = req.cookies?.token
+  if (!token) return res.status(401).send({ message: 'unauthorized access' })
+  jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: 'unauthorized access' })
+    }
+    req.user = decoded
+  })
+
+  next()
+}
+
 async function run() {
   try {
     const marathonCollection = client.db('marathonManagementSystem').collection('marathons')
     const usersCollection = client.db('marathonManagementSystem').collection('users')
+
+    // generate jwt
+    app.post('/jwt', async (req, res) => {
+      const email = req.body
+      // create token
+      const token = jwt.sign(email, process.env.SECRET_KEY, {
+        expiresIn: '365d',
+      })
+      console.log(token)
+      res
+        .cookie('token', token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production',
+          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        })
+        .send({ success: true })
+    })
+
+    // logout || clear cookie from browser
+    app.get('/logout', async (req, res) => {
+      res
+        .clearCookie('token', {
+          maxAge: 0,
+          secure: process.env.NODE_ENV === 'production',
+          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        })
+        .send({ success: true })
+    })
+
+    //get 6 marathons card for homepage from marathonCollection
+    app.get("/home_marathons", async (req, res) => {
+      const result = await marathonCollection.find().limit(6).toArray()
+      res.send(result);
+    });
 
     //get all marathons card from marathonCollection
     app.get('/marathons', async (req, res) => {
       const result = await marathonCollection.find().toArray()
       res.send(result)
     })
+
+    //users data
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+      const query = { email: newUser.email }
+      const alreadyExist = await bidsCollection.findOne(query)
+      if (alreadyExist)
+        return res
+          .status(400)
+          .send('You have already have an account!')
+      const result = await usersCollection.insertOne(newUser);
+      res.send(result);
+    });
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
