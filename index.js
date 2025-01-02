f33feee5de85387eef8fb1d5bf8986e22a622a40
Ridoy5315 <ridoy.st99@gmail.com>
2025-01-03 00:51:07 +0800

    updated registration count on data base

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
-const jwt = require('jsonwebtoken')
-const cookieParser = require('cookie-parser')
-require('dotenv').config()
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
 const port = process.env.PORT || 3000;
 
 const corsOptions = {
-  origin: ['http://localhost:5173'],
+  origin: ["http://localhost:5173"],
   credentials: true,
   optionalSuccessStatus: 200,
-}
+};
 
-app.use(cors(corsOptions))
-app.use(express.json())
-app.use(cookieParser())
-
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oggyj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -26,110 +25,141 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 // verifyToken
 const verifyToken = (req, res, next) => {
-  const token = req.cookies?.token
-  if (!token) return res.status(401).send({ message: 'unauthorized access' })
+  const token = req.cookies?.token;
+  if (!token) return res.status(401).send({ message: "unauthorized access" });
   jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
     if (err) {
-      return res.status(401).send({ message: 'unauthorized access' })
+      return res.status(401).send({ message: "unauthorized access" });
     }
-    req.user = decoded
-  })
+    req.user = decoded;
+  });
 
-  next()
-}
+  next();
+};
 
 async function run() {
   try {
-    const marathonCollection = client.db('marathonManagementSystem').collection('marathons')
-    const usersCollection = client.db('marathonManagementSystem').collection('users')
+    const marathonCollection = client
+      .db("marathonManagementSystem")
+      .collection("marathons");
+    const usersCollection = client
+      .db("marathonManagementSystem")
+      .collection("users");
+    const registeredCollection = client
+      .db("marathonManagementSystem")
+      .collection("registered-marathon");
 
     // generate jwt
-    app.post('/jwt', async (req, res) => {
-      const email = req.body
+    app.post("/jwt", async (req, res) => {
+      const email = req.body;
       // create token
       const token = jwt.sign(email, process.env.SECRET_KEY, {
-        expiresIn: '365d',
-      })
+        expiresIn: "365d",
+      });
       res
-        .cookie('token', token, {
+        .cookie("token", token, {
           httpOnly: true,
-          secure: process.env.NODE_ENV === 'production',
-          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
         })
-        .send({ success: true })
-    })
+        .send({ success: true });
+    });
 
     // logout || clear cookie from browser
-    app.get('/logout', async (req, res) => {
+    app.get("/logout", async (req, res) => {
       res
-        .clearCookie('token', {
+        .clearCookie("token", {
           maxAge: 0,
-          secure: process.env.NODE_ENV === 'production',
-          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
         })
-        .send({ success: true })
-    })
+        .send({ success: true });
+    });
 
     // save a jobData in db
-    app.post('/add-marathon', async (req, res) => {
-      const marathonData = req.body
-      const result = await marathonCollection.insertOne(marathonData)
-      res.send(result)
-    })
+    app.post("/add-marathon", async (req, res) => {
+      const marathonData = req.body;
+      const result = await marathonCollection.insertOne(marathonData);
+      res.send(result);
+    });
 
     //get 6 marathons card for homepage from marathonCollection
     app.get("/home_marathons", async (req, res) => {
-      const result = await marathonCollection.find().limit(6).toArray()
+      const result = await marathonCollection.find().limit(6).toArray();
       res.send(result);
     });
 
     //get all marathons card from marathonCollection
-    app.get('/marathons', async (req, res) => {
-      const result = await marathonCollection.find().toArray()
-      res.send(result)
-    })
+    app.get("/marathons", async (req, res) => {
+      const result = await marathonCollection.find().toArray();
+      res.send(result);
+    });
 
     // get a single marathon details data by id from db
-    app.get('/marathon/:id', async (req, res) => {
-      const id = req.params.id
-      const query = { _id: new ObjectId(id) }
-      const result = await marathonCollection.findOne(query)
-      res.send(result)
-    })
+    app.get("/marathon/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await marathonCollection.findOne(query);
+      res.send(result);
+    });
 
     //users data
     app.post("/users", async (req, res) => {
       const newUser = req.body;
-      const query = { email: newUser.email }
-      const alreadyExist = await bidsCollection.findOne(query)
+      const query = { email: newUser.email };
+      const alreadyExist = await usersCollection.findOne(query);
       if (alreadyExist)
+        return res.status(400).send("You have already have an account!");
+      const result = await usersCollection.insertOne(newUser);
+      res.send(result);
+    });
+
+    // save a registered marathon data in db
+    app.post("/add-registered-marathon", async (req, res) => {
+      const marathonData = req.body;
+      const query = {
+        "applicant.userEmail": marathonData.applicant.userEmail,
+        competition_id: marathonData.competition_id,
+      };
+      const alreadyExist = await registeredCollection.findOne(query);
+      if (alreadyExist){
         return res
           .status(400)
-          .send('You have already have an account!')
-      const result = await usersCollection.insertOne(newUser);
+          .send("You have already apply on this Competition!");
+      }
+
+      const result = await registeredCollection.insertOne(marathonData);
+
+      const filter = { _id: new ObjectId(marathonData.competition_id) }
+      const update = {
+        $inc: { registration_count: 1 },
+      }
+      const updateRegistrationCount = await marathonCollection.updateOne(filter, update)
       res.send(result);
     });
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-//     await client.close();
+    //     await client.close();
   }
 }
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-     res.send('Marathon management system project server side')
-})
+app.get("/", (req, res) => {
+  res.send("Marathon management system project server side");
+});
 
 app.listen(port, () => {
-     console.log(`Server is running at: ${port}`)
-})
+  console.log(`Server is running at: ${port}`);
+});
